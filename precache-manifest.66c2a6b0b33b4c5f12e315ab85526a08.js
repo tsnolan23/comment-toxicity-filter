733cd4b4219fcd294ccba36f28c712b391a8c4ea
Tom Nolan <tsnolan@outlook.com>
2019-05-26 11:25:18 -0500

    Updates

---
+++ precache-manifest.66c2a6b0b33b4c5f12e315ab85526a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ba2df422160c89f88ee2adb00d1680",
+    "url": "/comment-toxicity-filter/index.html"
+  },
+  {
+    "revision": "0ff736016dc475eeb76d",
+    "url": "/comment-toxicity-filter/static/css/main.78b66fc7.chunk.css"
+  },
+  {
+    "revision": "86316024e788f5693b92",
+    "url": "/comment-toxicity-filter/static/js/2.1af89c7e.chunk.js"
+  },
+  {
+    "revision": "0ff736016dc475eeb76d",
+    "url": "/comment-toxicity-filter/static/js/main.26c8163c.chunk.js"
+  },
+  {
+    "revision": "29ca3059b0a2e67f9d61",
+    "url": "/comment-toxicity-filter/static/js/runtime~main.c592c51d.js"
+  }
+]);

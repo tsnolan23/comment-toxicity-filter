8df8b463f667b1d7faca5bae2bdcd67c4392eca9
Tom Nolan <tsnolan@outlook.com>
2019-05-26 11:22:16 -0500

    feat: add github link

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import "./style.css";
 import CommentForm from "../CommentForm";
 import Intro from "../Intro";
+import GitHubLink from "../GitHubLink";
 
 const App = () => (
   <div className="App">
+    <GitHubLink />
     <Intro />
     <CommentForm onSubmit={() => alert("Your comment was not toxic!")} />
   </div>
